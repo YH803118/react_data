b47fccef8aefe2a09507fdac95b7ca1d825353c1
YH803118 <dudghks1045@naver.com>
2022-11-09 15:05:16 +0900

    controll Component

--- src/component/ReviewForm.js
+++ src/component/ReviewForm.js
@@ -1,15 +1,39 @@
 import { useState } from "react";
 
+// 제어 컴포넌트 방식
 function ReviewForm() {
-  const [title, setTitle] = useState("");
+  const [values, setValues] = useState({
+    title: "",
+    rating: 0,
+    content: "",
+  });
 
-  const handleTitleChange = (e) => {
-    setTitle(e.target.value);
+  const handleChange = (e) => {
+    const { name, value } = e.target;
+    setValues((preValues) => ({
+      ...preValues,
+      [name]: value,
+      // 대괄호표기 : 프로퍼티명으로 지?정해줄 수 있음
+    }));
+  };
+
+  const handleReivewSubmit = (e) => {
+    e.preventDefault();
+    // preventDefault : 이벤트 객체의 기본 동작을 막는다
+    console.log(values);
   };
 
   return (
-    <form>
-      <input value={title} onChange={handleTitleChange}></input>
+    <form className="ReviewForm" onSubmit={handleReivewSubmit}>
+      <input name="title" value={values.title} onChange={handleChange} />
+      <input
+        name="rating"
+        type="number"
+        value={values.rating}
+        onChange={handleChange}
+      />
+      <textarea name="content" value={values.content} onChange={handleChange} />
+      <button type="submit">전송</button>
     </form>
   );
 }
